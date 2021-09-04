94ec9f8df43ca86b60053a58aee9436a6e7df1fa
AgustinZavalaA <1930120@upv.edu.mx>
2021-09-04 07:36:52 -0500

    Serial pruebas

--- ArduinoSerialComm.py
+++ ArduinoSerialComm.py
@@ -2,16 +2,16 @@ import serial
 import time
 
 if __name__ == "__main__":
-    ser = serial.Serial("/dev/ttyACM0", 115200, timeout=1)
+    ser = serial.Serial("/dev/ttyACM0", 115200, timeout=0.1)
     ser.flush()
     time.sleep(3)
     try:
         while True:
             ser.write(b"1")
-            time.sleep(0.25)
+            time.sleep(0.1)
             # works with sleep 1
             line = ser.readline().decode("ascii").rstrip()
 
-            print("u1 = " + line)
+            print(line.split(","))
     except KeyboardInterrupt:
         ser.close()
