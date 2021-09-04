c26878e23a1ffcf32270e3e61f7aea54ae7a7d6f
AgustinZavalaA <1930120@upv.edu.mx>
2021-09-04 08:37:23 -0500

    rpc server con arduino communication

--- rpc_server.py
+++ rpc_server.py
@@ -1,3 +1,4 @@
+from typing import List, Tuple
 from xmlrpc.server import SimpleXMLRPCServer
 from Motors import Motors
 from ArduinoSerialComm import ArduinoComm
@@ -7,6 +8,8 @@ import time
 class ServerObjects:
     motors = Motors()
     arduino = ArduinoComm()
+    time.sleep(2)
+    print("Arduino y motores listos")
 
     def move_motors(self, motor: bool, speed: int, direction: bool) -> None:
         self.motors.move(motor, speed, direction)
@@ -17,12 +20,18 @@ class ServerObjects:
     def stop_motors(self) -> None:
         self.motors.stop()
 
+    def communicate_arduino(self, data: str = "1") -> Tuple[int, int, List[str]]:
+        return self.arduino(data)
+
+    def close_arduino(self) -> None:
+        self.arduino.close()
+
 
 # set up the server
 server = SimpleXMLRPCServer(("192.168.0.10", 8000), allow_none=True, logRequests=False)
 
 # register our functions
-server.register_instance(DriverMotors())
+server.register_instance(ServerObjects())
 # Run the server's main loop
 try:
     server.serve_forever()
