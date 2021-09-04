f423ed8258eec14db2adc1f3fc47e4643932189e
AgustinZavalaA <1930120@upv.edu.mx>
2021-09-04 08:05:52 -0500

    Serial ahora es una clase sin probar

--- ArduinoSerialComm.py
+++ ArduinoSerialComm.py
@@ -1,17 +1,49 @@
+from typing import List, Tuple
 import serial
 import time
 
-if __name__ == "__main__":
-    ser = serial.Serial("/dev/ttyACM0", 115200, timeout=0.1)
-    ser.flush()
+
+class ArduinoComm:
+    def __init__(self, port: str = "/dev/ttyACM0", baudrate: int = 115200, timeout: float = 0.1) -> None:
+        self.ser = serial.Serial(port, baudrate, timeout=timeout)
+        self.ser.flush()
+
+    def communicate(self, data: str = "1") -> Tuple[int, int, List[str]]:
+        self.ser.write(data.encode("ascii"))
+        # time.sleep(0.1)
+        # works with sleep 1
+        line = self.ser.readline().decode("ascii").rstrip()
+        line_list = line.split(",")
+        return int(line_list[0]), int(line_list[0]), line_list[2:]
+
+    def close(self):
+        self.ser.close()
+
+
+def main():
+    arduino = ArduinoComm()
     time.sleep(3)
+
     try:
         while True:
-            ser.write(b"1")
+            print(arduino.communicate())
             time.sleep(0.1)
-            # works with sleep 1
-            line = ser.readline().decode("ascii").rstrip()
-
-            print(line.split(","))
     except KeyboardInterrupt:
-        ser.close()
+        arduino.close()
+
+
+if __name__ == "__main__":
+    # ser = serial.Serial("/dev/ttyACM0", 115200, timeout=0.1)
+    # ser.flush()
+    # time.sleep(3)
+    # try:
+    #     while True:
+    #         ser.write(b"1")
+    #         time.sleep(0.1)
+    #         # works with sleep 1
+    #         line = ser.readline().decode("ascii").rstrip()
+
+    #         print(line.split(","))
+    # except KeyboardInterrupt:
+    #     ser.close()
+    main()
