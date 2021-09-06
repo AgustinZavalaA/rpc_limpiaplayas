4375b70c394912e871e2d6f3a1ef208aa9ccde53
AgustinZavalaA <1930120@upv.edu.mx>
2021-09-06 02:01:47 -0500

    Archivos ordenados dentro de una jerarquia de carpetas

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-for event in pygame.event.get():
-            if event.type == pygame.JOYBUTTONDOWN:
-                print("Joystick button pressed.")
-            if event.type == pygame.JOYBUTTONUP:
-                print("Joystick button released.")
