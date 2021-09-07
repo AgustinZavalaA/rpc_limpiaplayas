0d6044ee4a1beb344fb3d357f54e88b82788c3f4
AgustinZavalaA <1930120@upv.edu.mx>
2021-09-06 20:16:32 -0500

    multiprocessing?????

--- Python/CameraProcessing.py
+++ Python/CameraProcessing.py
@@ -1,5 +1,6 @@
 from picamera.array import PiRGBArray
 from picamera import PiCamera
+from multiprocessing import Pipe, Process
 import time
 import cv2
 
@@ -15,7 +16,7 @@ class CameraProcessing:
         # allow the camera to warmup
         time.sleep(0.1)
 
-    def process_video(self):
+    def process_video(self, conn: Pipe) -> None:
         # capture frames from the camera
         for frame in self.stream:
             # grab the raw NumPy array representing the image, then initialize the timestamp
@@ -25,15 +26,21 @@ class CameraProcessing:
             cv2.imshow("Frame", image)
             key = cv2.waitKey(1) & 0xFF
             # clear the stream in preparation for the next frame
+            conn.send(image[0, 0, 0])
             self.raw_capture.truncate(0)
             # if the `q` key was pressed, break from the loop
             if key == ord("q"):
+                conn.close()
                 break
 
 
 def main() -> None:
     camera = CameraProcessing()
-    camera.process_video()
+    parent_conn, child_conn = Pipe()
+    p = Process(target=camera.process_video, args=(child_conn,))
+    p.start()
+    print(parent_conn.recv())
+    p.join()
 
 
 if __name__ == "__main__":
