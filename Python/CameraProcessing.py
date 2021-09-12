c1ad4590d6ed44e96be525e59003ce7c6f700365
AgustinZavalaA <1930120@upv.edu.mx>
2021-09-11 19:50:16 -0500

    modificado tamaño camara

--- Python/CameraProcessing.py
+++ Python/CameraProcessing.py
@@ -65,7 +65,7 @@ class CameraProcessing:
 
 
 def process_video_detect_mp_function(child_conn: Pipe):
-    camera = CameraProcessing(show=True, resolution=(320, 240))
+    camera = CameraProcessing(show=True)
     for frame in camera.stream:
         key, detect_result = camera.process_video_detect(frame)
         child_conn.send(detect_result)
